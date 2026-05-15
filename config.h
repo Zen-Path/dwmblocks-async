230a779fda48f96437b575f39116a8c0a298d985
ZenPath <zen.path@tuta.com>
2026-05-15 23:30:41 +0300

    cf: call sb-network instead of sb-internet

--- config.h
+++ config.h
@@ -22,7 +22,7 @@
     X("", "sb-date",        15,        1) \
     /* X("", "sb-battery",     15,        2) */ \
     X("", "sb-volume",      0,         3) \
-    X("", "sb-internet",    15,        4) \
+    X("", "sb-network",    15,        4) \
     X("", "sb-tasks",       60 * 1,    5) \
     X("", "sb-news",        60 * 5,    6) \
     /* X("", "sb-mail",        60 * 5,    7) */ \
